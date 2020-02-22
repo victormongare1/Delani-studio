f36f22fcce575ee3b1b4f8cafbd4668f78e6e067
victormongare1 <vicmongz254@gmail.com>
2020-02-22 20:44:53 +0300

    added hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,52 @@ $(document).ready(function(){
     $(".product-showing").show();
     $(".product-hidden").hide();
   });
-});
+    $("#img1").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img2").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img3").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img4").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img5").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img6").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img7").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+    $("#img8").hover(function () {
+    $(this).animate({opacity:'0.5'});
+    },
+    function () {
+    $(this).animate({opacity:'1'});
+  })
+})
